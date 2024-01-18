4bce13e1daaddff12fc9256d163ee4c791ff71ff
Tafara96 <151039190+Tafara96@users.noreply.github.com>
2024-01-18 17:10:03 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+  "github.com/Tafara96/estiam-main/routes"
+	"github.com/Tafara96/estiam-main/dictionary"
+)
+
+func main() {
+	//Création d'un nouveau dictionnaire avec un nom de fichier
+  myDictionary := dictionary.New("dictionary.txt")
+
+  //Mise en place des routes
+  router := route.SetupRoutes(myDictionary)
+
+  //Demmarer le serveur
+	fmt.Sprintf("Serveur Démarré")
+  http.Handle("/", router)
+  http.ListenAndServe(":8080", nil)
+}
+
+/*func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
+
+}
+
+func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
+
+}
+
+func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
+
+}
+
+func actionList(d *dictionary.Dictionary) {
+
+}*/
